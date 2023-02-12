57170cb95801607646dede0accbfaf67ec92f53b
brenofflank <107095857+Klackters@users.noreply.github.com>
2023-02-12 14:17:23 -0300

    up temp

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import ToolBar from './components/ToolBar';
+
+function App() {
+  return (
+    <ToolBar />
+  );
+}
+
+export default App;
---
+++ src/components/ToolBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Toolbar } from 'primereact/toolbar';
+import './ToolBar.css';
+
+export default function ToolBar() {
+    return(
+        <div>
+            <Toolbar className='toolbar'>
+
+            </Toolbar>
+        </div>
+    )
+}
+
